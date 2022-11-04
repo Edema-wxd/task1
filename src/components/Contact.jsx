380d7d1efb2b421704b9de1535ef98b63a2079a9
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-11-04 23:33:10 +0100

    style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,36 +1,42 @@
 
 function Contact() {
     return (
-        <>
-        <h2>Contact Me</h2>
-        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        <div className="contact">
+            <h2>Contact Me</h2>
+            <p>Hi there, contact me to ask me about anything you have in mind.</p>
 
-        <form>
-            <div className="cont name">
-                <label htmlFor="first_name">First Name</label>
+            <form>
+                <div className="cont name">
+                    <div className="c">
+                        <label htmlFor="first_name">First Name</label>
+                        <br />
+                        <input className="in"  type="text" name="First Name" id="first_name" placeholder="Enter your first name"/>
+                    </div>
+
+                    <div className="d">
+                        <label htmlFor="last_name">Last Name</label>
+                        <br />
+                        <input  className="in" type="text" name="Last Name" id="last_name" placeholder="Enter your last name" />
+                    </div>
+                </div>
+
+                <label htmlFor="email">Email</label>
                 <br />
-                <input type="text" name="First Name" id="first_name" placeholder="Enter your first name"/>
+                <input  className="in" type="email" name="email" id="email" placeholder="yourname@email.com"/>
 
-                <label htmlFor="last_name">Last Name</label>
+                <br/>
+
+                <label htmlFor="message">Message</label>
                 <br />
-                <input type="text" name="Last Name" id="last_name" placeholder="Enter your last name" />
-            </div>
+                <textarea className="txtbxt conta" name="message" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
+                <br/>
+                <input type="checkbox" name="consent" id="consent" />
+                <p>You agree to providing your data to Francis who may contact you.</p>
 
-            <label htmlFor="email">Email</label>
-            <br />
-            <input type="email" name="email" id="email" placeholder="yourname@email.com"/>
+                <button className="ctn" id="btn__submit" type="submit">Send Message</button>
 
-            <label htmlFor="message">Message</label>
-            <br />
-            <textarea name="message" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..."></textarea>
-
-            <input type="checkbox" name="consent" id="consent" />
-            <p>You agree to providing your data to Francis who may contact you.</p>
-
-            <button id="btn__submit" type="submit">Send Message</button>
-
-        </form>
-        </>
+            </form>
+        </div>
     )
 }
 
