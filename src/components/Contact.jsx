23d6e588dd082930a9de61612303a923a9cc0354
Edema-wxd <Edema-wxd@users.noreply.github.com>
2022-11-05 12:54:16 +0100

    fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,74 @@
+import { useState } from "react"
+
 
 function Contact() {
+    const [check, setCheck] = useState()
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [fnText, setFNText] = useState('')
+    const [lnText, setLNText] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+    const [errfnText, seterrFNText] = useState('')
+    const [errlnText, seterrLNText] = useState('')
+    const [erremail, seterrEmail] = useState('')
+    const [errmessage, seterrMessage] = useState('')
+
+    const handleCheck = () =>{
+        if (check === true) {
+            setBtnDisabled(false)
+        }
+        else(
+            setCheck('Please consent to me contacting you')
+        )
+        
+    }
+
+    const handleFNChange = (a) =>{
+        if ( fnText === ''){
+            seterrFNText(null)
+        } else if (fnText !== '' && fnText.trim().length <= 5) {
+            seterrFNText('Please type your first name')
+            
+        } else{
+            seterrFNText(null)
+        }
+        setFNText(a.target.value)
+    }
+    const handleLNChange = (a) =>{
+        if ( lnText === ''){
+
+        } else if (lnText !== '' && lnText.trim().length <= 5) {
+            seterrLNText('Please type your last name')
+        } else{
+            seterrLNText(null)
+        }
+        setLNText(a.target.value)
+    }
+    const handleEmailChange = (a) =>{
+        if ( email === ''){
+            seterrEmail(null)
+        } else if (email !== '' && email.trim().length <= 5) {
+            seterrEmail('Enter a valid email')
+        } else{
+            seterrEmail(null)
+        }
+        setEmail(a.target.value)
+    }
+    const handleMessageChange = (a) =>{
+        if ( message === ''){
+            seterrMessage(null)
+        } else if (message !== '' && message.trim().length <= 5) {
+            seterrMessage('Please enter a message')
+            
+        } else{
+            seterrMessage(null)
+        }
+        setMessage(a.target.value)
+    }
+
+
+
     return (
         <div className="contact">
             <h2>Contact Me</h2>
@@ -9,35 +78,40 @@ function Contact() {
                 <div className="cont name">
                     <div className="c">
                         <label htmlFor="first_name">First Name</label>
-                        <input className="in"  type="text" name="First Name" id="first_name" placeholder="Enter your first name" required/>
+                        <input onChange={handleFNChange} className="in" value={fnText}  type="text" name="First Name" id="first_name" placeholder="Enter your first name" required/>
+                        {errfnText && <div className="errmsg">{errfnText} </div>}
                     </div>
 
                     <div className="d">
                         <label htmlFor="last_name">Last Name</label>
-                        <input  className="in" type="text" name="Last Name" id="last_name" placeholder="Enter your last name" required/>
+                        <input onChange={handleLNChange}  className="in" value={lnText} type="text" name="Last Name" id="last_name" placeholder="Enter your last name" required/>
+                        {errlnText && <div className="errmsg">{errlnText} </div>}
                     </div>
                 </div>
 
                 <label htmlFor="email">Email</label>
-                <input  className="in" type="email" name="email" id="email" placeholder="yourname@email.com" required/>
+                <input onChange={handleEmailChange}  className="in" value={email}type="email" name="email" id="email" placeholder="yourname@email.com" required/>
+                {erremail && <div className="errmsg">{erremail} </div>}
 
 
                 <label htmlFor="message">Message</label>
-                <textarea className="conta txtbxt" name="message" id="message" wrap="soft" placeholder="Send me a message and I'll reply you as soon as possible..." spellCheck required ></textarea>
+                <textarea onChange={handleMessageChange}  className="conta txtbxt" value={message} name="message" id="message" wrap="soft" placeholder="Send me a message and I'll reply you as soon as possible..." spellCheck required ></textarea>
+                {errmessage && <div className="errmsg">{errmessage} </div>}
 
                 <br />
                 <div className="chk">
 
                     <label class="control control-checkbox">
                         You agree to providing your data to Francis who may contact you.
-                            <input type="checkbox" name="consent" id="consent" requiredchecked="checked" />
+                            <input onChange={handleCheck} type="checkbox" name="consent" id="consent" required checked="checked" value={check} />
                         <div class="control_indicator"></div>
                     </label>
+                    {check && <div className="errmsg">{check} </div>}
                 
                 </div>
                 
 
-                <button className="ctn" id="btn__submit" type="submit">Send Message</button>
+                <button disabled={btnDisabled} className="ctn" id="btn__submit" type="submit">Send Message</button>
 
             </form>
         </div>
